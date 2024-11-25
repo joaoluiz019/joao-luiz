40ad7da8d66f120372a1eb93967f31e315311e38
joaoluiz019 <joloo2017@gmail.com>
2024-11-24 22:44:41 -0300

    feat/ alteração tela inicio

--- portifolio/src/app/page.tsx
+++ portifolio/src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
         >
           <div className="text-center">
             <h1 className="text-5xl md:text-7xl font-bold mb-4">João Luiz Ricardo Esteves</h1>
-            <p className="text-xl md:text-2xl">Desenvolvedor Web Full Stack</p>
+            <p className="text-xl md:text-2xl">Desenvolvedor Backend</p>
           </div>
         </motion.section>
 
